37ed26b96d2df064a514bee02d342c89774cf595
Carlos Alberto Souza Nascimento <carlosalbertosn.10@gmail.com>
2024-05-20 16:58:16 -0300

    Alteração classe doações

--- src/model/entities/Doacao.java
+++ src/model/entities/Doacao.java
@@ -2,4 +2,31 @@ package model.entities;
 
 public class Doacao {
 
+    private String nome;
+    private String tipo;
+    private String genero;
+    private String tamanho;
+
+    // Construtor sem passagem de parâmetros
+    public Doacao() {
+
+    }
+
+    // Construtor com passagem de parâmetros
+    public Doacao(String nome, String tipo, String genero, String tamanho) {
+        this.nome = nome;
+        this.tipo = tipo;
+        this.genero = genero;
+        this.tamanho = tamanho;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Itens: " +
+                "nome='" + nome + '\'' +
+                ", tipo='" + tipo + '\'' +
+                ", genero='" + genero + '\'' +
+                ", tamanho='" + tamanho + '\'' +
+                '}';
+    }
 }
