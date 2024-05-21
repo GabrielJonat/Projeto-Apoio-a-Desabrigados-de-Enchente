b58b901553edd29561b493ba52c3578f113dd170
Thiagod3 <thiago.vilela3@outlook.com>
2024-05-21 09:40:19 -0300

    setup transf class

--- src/model/entities/Transferencia.java
+++ src/model/entities/Transferencia.java
@@ -1,5 +1,48 @@
 package model.entities;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Transferencia {
+    Scanner sc = new Scanner(System.in);
+
+    Connection conn = null;
+    Statement st = null;
+    ResultSet rs = null;
+
+    public void checkout(){
+
+        for (int i=0; i<n; i++){
+
+            try {
+                conn = DB.getConnection();
+                st = conn.createStatement();
+                rs = st.executeQuery("select * from department");
+
+                while (rs.next()) {
+                    System.out.println(rs.getInt("Id"));
+                    System.out.println("Deseja aceitar ou recusar a requisição acima? Escreva S para aceitar e N para negar requisição");
+                    sc.getString();
+                }
+            }
+            catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        finally {
+            DB.closeResultSet(rs);
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
+
+
+    }
+
+    sc.close();
 
 }
