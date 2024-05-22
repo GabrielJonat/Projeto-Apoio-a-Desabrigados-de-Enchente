786855195b58857ff2954432e29b70abd04d09f5
Thiagod3 <thiago.vilela3@outlook.com>
2024-05-22 18:57:14 -0300

    merge abrigos + update menu

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,37 +1,112 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.Scanner;
 
-import db.DB;
+import model.dao.AbrigoDao;
+import model.dao.DaoFactory;
+import application.UI.*;
 
 public class Programa {
-
+		
 	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
 		
-		Connection conn = DB.getConnection();
-		
-		ResultSet rs = null;
+		AbrigoDao abrigoDao = DaoFactory.createSellerDao();
 		
 		
-		try {
-			
-			Statement st = conn.createStatement();
+		int escolha = 1;
+		boolean sair = true;
 		
-			rs = st.executeQuery("select * from centro");
-			
-			while(rs.next()) {
+		clear();
+		
+		while(sair) {
+				menu();
 				
-				System.out.println(rs.getInt("id") + ", " + rs.getString("nome") + ", " + rs.getString("logradouro") + ", " + rs.getInt("numero") + ", " + rs.getString("cep") + ", " + rs.getString("bairro") + ", " + rs.getString("estado") + ", " + rs.getString("cidade") + ", " +  rs.getInt("limite"));
-							
-			}
-			
-		}
-		catch(SQLException e) {
-			
-			System.out.println(e.getMessage());
-		}
+				escolha = sc.nextInt();
+					
+				switch(escolha) {
+				
+				case 1:
+					
+					abrigosUI.a(sc, abrigoDao);
+					
+					for(int i = 0; i < 2; i++)
+						sc.nextLine();
+					
+					break;
+					
+				case 2:
+					
+					clear();
+					//Doacoes
+					break;
+					
+				case 3:
+					
+					clear();
+					//Ordem de pedido
+					break;
+					
+				case 4: 
+					
+					clear();
+					//Checkout
+					break;
+					
+				case 5:
+					
+					clear();
+					//Transferencia
+					break;
+					
+				case 6:
+					
+					clear();
+					sair = false;
+					break;
+					
+				default:
+					
+					clear();
+					System.out.println("Essa opção não existe, escolha uma entre 1 e 6.\n");					
+					break;
+					
+				}
+		}	
+		
+		System.out.println("Tchau!");
+		
+		sc.close();
+		
 	}
+	
+	public static void menu() {
+
+		System.out.println("+-------------------------------+");
+		System.out.println("|                               |");
+		System.out.println("|       CENTRO DE DOAÇÕES       |");
+		System.out.println("|                               |");
+		System.out.println("|           COMPASS.UOL         |");
+		System.out.println("|                               |");
+		System.out.println("|                               |");
+		System.out.println("| Informe a opção desejada:     |");
+		System.out.println("|                               |");
+		System.out.println("|    1 - Abrigos                |");
+		System.out.println("|    2 - Doações                |");
+		System.out.println("|    3 - Ordem de pedido        |");
+		System.out.println("|    4 - Checkout de itens      |");
+		System.out.println("|    5 - Tranferência de doações|");
+		System.out.println("|    6 - Sair do programa       |");
+		System.out.println("|                               |");
+		System.out.println("+-------------------------------+");
+		System.out.print("\nDigite sua opção: ");
+	}
+	
+	public static void clear() {
+	    for (int i = 0; i < 50; i++) {
+	        System.out.println();
+	    }
+	}
+
 }
--- src/model/entities/Abrigo.java
+++ src/model/entities/Abrigo.java
@@ -1,47 +1,32 @@
 package model.entities;
 
-import java.util.Objects;
-
 public class Abrigo {
-	
-	private int id;
+
+	private Integer id = null;
 	private String nome;
-	private String endereco;
+	private String logradouro;
+	private Integer numero;
 	private String responsavel;
 	private String telefone;
 	private String email;
-	private int capacidade;
-	private double ocupacao;
+	private Integer capacidade = 200;
+	private Double ocupacao = null;
+	private Integer numOcupacao;
 	
 	public Abrigo() {}
 	
-	public Abrigo(String nome, String endereco, String responsavel, String telefone, String email, int capacidade,
-			double ocupacao) {
+	public Abrigo(Integer id, String nome, String responsavel, String logradouro, Integer numero, String telefone, String email, Integer numOcupacao) {
 		
+		this.id = null;
 		this.nome = nome;
-		this.endereco = endereco;
 		this.responsavel = responsavel;
 		this.telefone = telefone;
+		this.logradouro = logradouro;
+		this.numero = numero;
 		this.email = email;
-		this.capacidade = capacidade;
-		this.ocupacao = ocupacao;
-	
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getEndereco() {
-		return endereco;
-	}
-
-	public void setEndereco(String endereco) {
-		this.endereco = endereco;
+		this.numOcupacao = numOcupacao;
+		this.ocupacao = null;
+		
 	}
 
 	public String getResponsavel() {
@@ -68,54 +53,72 @@ public class Abrigo {
 		this.email = email;
 	}
 
-	public int getCapacidade() {
+	public Integer getCapacidade() {
 		return capacidade;
 	}
 
-	public void setCapacidade(int capacidade) {
+	public void setCapacidade(Integer capacidade) {
 		this.capacidade = capacidade;
 	}
 
 	public double getOcupacao() {
 		return ocupacao;
 	}
-
-	public void setOcupacao(double ocupacao) {
+	
+	public void setOcupacao(Double ocupacao) {
+		
 		this.ocupacao = ocupacao;
 	}
-	
+
 	
 
-	public int getId() {
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public Integer getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+
+	public Integer getNumOcupacao() {
+		return numOcupacao;
+	}
+
+	public void setNumOcupacao(Integer numOcupacao) {
+		this.numOcupacao = numOcupacao;
+	}
+
+	
+		
+	public Integer getId() {
 		return id;
 	}
 
-	public void setId(int id) {
+	public void setId(Integer id) {
 		this.id = id;
 	}
 
 	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Abrigo other = (Abrigo) obj;
-		return id == other.id;
-	}
-
-	@Override
 	public String toString() {
-		return "Abrigo [id=" + id + ", nome=" + nome + ", endereco=" + endereco + ", responsavel=" + responsavel
-				+ ", telefone=" + telefone + ", email=" + email + ", capacidade=" + capacidade + ", ocupacao="
-				+ ocupacao + "]";
+		return "Abrigo [id= " + id + ", nome= " + nome + ", responsavel= " + responsavel
+				+ ", telefone= " + telefone + ", email= " + email + ", capacidade= " + capacidade + ", ocupacao= "
+				+ ocupacao + "%]";
 	}
 	
 	
