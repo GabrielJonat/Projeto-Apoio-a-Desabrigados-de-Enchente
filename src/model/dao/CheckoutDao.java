fc2d3a4500af019c75bd13e8d00bb08f3dd58175
Jonathan <jonathanyanagita@gmail.com>
2024-05-24 20:51:12 -0300

    Feat: update methods

--- src/model/dao/CheckoutDao.java
+++ src/model/dao/CheckoutDao.java
@@ -1,5 +1,10 @@
 package model.dao;
 
 public class CheckoutDao {
+	
+	//atualizar tabela de checkout
+	void updateStatus(Integer id, Boolean status); //atualizar tabela de checkout mudando para true ou false
+	void updateAbrigo(Integer id, Integer quantidade); //atualizar tabela de estoque calculando qtdds de item no abrigo
+	void updateCentro(Integer id, Integer quantidade); //atualizar tabela de estoque subtraindo qtdds de item do centro
 
 }
--- src/model/dao/impl/CheckoutDaoJDBC.java
+++ src/model/dao/impl/CheckoutDaoJDBC.java
@@ -1,5 +1,97 @@
 package model.dao.impl;
 
-public class CheckoutDaoJDBC {
+package model.dao.impl;
 
-}
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import db.DB;
+import db.DbException;
+import model.dao.CheckoutDao;
+import model.dao.estoqueCentro;
+import model.entities.Abrigo;
+import model.entities.Checkout;
+import model.entities.Item;
+import model.entities.Pedido;
+
+public class CheckoutDaoJDBC implements CheckoutDao{
+	
+	private Connection conn;
+	
+	public CheckoutDaoJDBC(Connection conn) {
+		this.conn = conn;
+	}	
+	
+	@Override
+	public void UpdateStatus(Integer id, Boolean status) {
+		
+		PreparedStatement st = null;
+		
+		try {
+			st = conn.prepareStatement("UPDATE checkout SET status_pedido=? WHERE id=?");
+			
+			st.setInt(1, status);
+			st.setInt(2, id);
+
+			int rowsAffected = st.executeUpdate();
+			
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
+	}
+	
+		
+	@Override
+	public void updateAbrigo(Integer idAbrigo, Integer idItem , Integer quantidade) {
+		
+		PreparedStatement st = null;
+		
+		try {
+			st = conn.prepareStatement("UPDATE estoqueAbrigo SET quantidade=quantidade+? WHERE id_abrigo=? AND id_item=?");
+			
+			st.setInt(1, quantidade);
+			st.setInt(2, idAbrigo);
+			st.setInt(3, idItem);
+
+			int rowsAffected = st.executeUpdate();
+			
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
+	}
+	
+	
+	@Override
+	public void updateCentro(Integer idCentro, Integer idItem , Integer quantidade) {
+		
+		PreparedStatement st = null;
+		
+		try {
+			st = conn.prepareStatement("UPDATE estoqueCentro SET quantidade=quantidade-? WHERE idCentro=? AND idItem=?");
+			
+			st.setInt(1, quantidade);
+			st.setInt(2, idCentro);
+			st.setInt(3, idItem);
+
+			int rowsAffected = st.executeUpdate();
+			
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
+	}
